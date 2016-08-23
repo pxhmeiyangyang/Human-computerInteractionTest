75220418c6e4bc8b8afd7a9b1cf2232963385392
pxh <peixinhua@unisound.com>
2016-08-23 10:46:35 +0800

    第一次工程提交

---
+++ Human-computerInteractionTest/Human-computerInteractionTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Human-computerInteractionTest
+//
+//  Created by pxh on 16/8/23.
+//  Copyright © 2016年 裴新华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Human-computerInteractionTest/Human-computerInteractionTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Human-computerInteractionTest
+//
+//  Created by pxh on 16/8/23.
+//  Copyright © 2016年 裴新华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
